b2a93aaea6044d27f7d49ca521cbbea8c72e890b
MASINGITA OTTIS MALULEKE <masingitaottismaluleke@gmail.com>
2024-10-02 20:49:48 -0700

    Updates

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,8 +1,3 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // The promise doesn't do anything specific, it just needs to be a promise
-	  setTimeout(() => {
-      resolve('Data from fake API');
-    }, 1000);
-  });
+  return new Promise(() => {});
 }
