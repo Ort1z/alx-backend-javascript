5ddbb2b2491987164166e085864dce2cc747a254
MASINGITA OTTIS MALULEKE <38721983+Ort1z@users.noreply.github.com>
2024-10-04 18:53:09 +0200

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
-    let innerTask = true; // Declare a new variable with 'let'
-    let innerTask2 = false; // Declare a new variable with 'let'
-    return [innerTask, innerTask2]; // Return the inner variables
+    var task = true;
+    var task2 = false;
   }
 
   return [task, task2];
